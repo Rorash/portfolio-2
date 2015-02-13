125215d91205f053c4680d8da7f0eba506a8d899
Alexander Sokolov <Sokolov.link@gmail.com>
2015-02-14 01:07:13 +0300

    clear with gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+rename = require('gulp-rename'),
+minifyCSS = require('gulp-minify-css'),
+autoprefixer = require('gulp-autoprefixer'),
+livereload = require('gulp-livereload'),
+server = require('gulp-server-livereload');
+ 
+gulp.task('sass', function () {
+    gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(rename("style.css"))
+        .pipe(minifyCSS())
+        .pipe(autoprefixer(['last 2 versions']))
+        .pipe(gulp.dest('./'));
+
+});
+
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(server({
+      livereload: true,
+      open: true,
+      defaultFile: 'index.html'
+    }));
+});
+
+
+gulp.task('watch', function () {
+	gulp.watch('./scss/*.scss', ['sass'])
+	gulp.watch('./scss/**/*.scss', ['sass'])
+});
+gulp.task('default', ['watch','webserver']);
