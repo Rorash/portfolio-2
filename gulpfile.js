ab9318f6fb40b5052cd3d624e48157cfef9e5d98
Alexander Sokolov <Sokolov.link@gmail.com>
2015-02-20 21:12:41 +0300

    modern with bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,25 @@ rename = require('gulp-rename'),
 minifyCSS = require('gulp-minify-css'),
 autoprefixer = require('gulp-autoprefixer'),
 livereload = require('gulp-livereload'),
-server = require('gulp-server-livereload');
+server = require('gulp-server-livereload'),
+compass = require( 'gulp-for-compass' );
  
 gulp.task('sass', function () {
     gulp.src('./scss/style.scss')
-        .pipe(sass())
+        .pipe(compass({
+            sassDir: 'scss',
+            cssDir: '.',
+            imagesDir: 'img',
+            javascriptsDir: 'js',
+            force: true
+        }))
         .pipe(rename("style.css"))
-        .pipe(minifyCSS())
-        //.pipe(autoprefixer(['last 2 versions']))
+        //.pipe(minifyCSS())
+        .pipe(autoprefixer(['last 2 versions']))
         .pipe(gulp.dest('./'));
 
 });
+
 
 gulp.task('webserver', function() {
   gulp.src('.')
